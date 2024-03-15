56df9206f2297ea11630e1d051276aaa36562451
Katy248 <petrovanton247@gmail.com>
2024-03-15 12:25:11 +0300

    Full refactor

--- install.sh
+++ install.sh
@@ -3,19 +3,42 @@ install_package() {
 	dnf install -y --skip-broken $1
 }
 
-
 # update
+
 dnf makecache
 dnf upgrade -y
-packages=(gimp firefox steam yandex-browser-stable r7-office)
+
+# adding custom repositories
+
+## adding skype repository
+cat > /etc/yum.repos.d/microsoft-skype.repo << "EOF"
+[microsoft-skype]
+name=microsoft-skype
+baseurl=https://repo.skype.com/rpm/stable/
+enabled=1
+gpgcheck=0
+EOF
+
+## adding masterPDFeditor repository
+dnf config-manager --add-repo http://repo.code-industry.net/rpm/master-pdf-editor.repo
+## adding skype repository
+dnf config-manager --add-repo https://repo.skype.com/rpm/stable/
+## adding ms edge repository 
+dnf config-manager --ad-repo https://packages.microsoft.com/yumrepos/edge/config.repo
+
+
 
 # install software in repos
+
+packages=(gimp firefox steam yandex-browser-stable r7-office master-pdf-editor microsoft-edge-stable skypeforlinux)
+
 for pak in $packages
 do
 	install_package $pak
 done
 
 # install software as .rpm file
+
 chrome='https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm'
 zoom='https://zoom.us/client/5.17.11.3835/zoom_x86_64.rpm'
 chromegost='https://github.com/deemru/Chromium-Gost/releases/download/122.0.6261.112/chromium-gost-122.0.6261.112-linux-amd64.rpm'
@@ -30,32 +53,6 @@ do
 	install_package $pak
 done
 
-# install software from other repositories
-
-# adding ms edge repository
-cat > /etc/yum.repos.d/microsoft-edge.repo << "EOF"
-[microsoft-edge]
-name=microsoft-edge
-baseurl=https://packages.microsoft.com/yumrepos/edge/
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc
-EOF
-
-# adding masterPDFeditor repository
-dnf config-manager --add-repo http://repo.code-industry.net/rpm/master-pdf-editor.repo
-# adding skype repository
-dnf config-manager --add-repo https://repo.skype.com/rpm/stable/
-
-echo "Start installing AnyDesk, Master PDF editor, MS Edge, Skype"
-
-custom_packages=(master-pdf-editor microsoft-edge-stable skypeforlinux)
-
-for pak in $custom_packages
-do
-	install_package $pak
-done
-	
 
 dnf makecache
 dnf upgrade -y
