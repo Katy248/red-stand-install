b77c786ad2f958266220a5b4d1a5223114c3348b
Katy248 <petrovanton247@gmail.com>
2024-06-17 16:44:12 +0300

    Fix forgotten flatpak installation

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ chromegost='https://github.com/deemru/Chromium-Gost/releases/download/122.0.6261
 myoffice='https://preset.myoffice-app.ru/myoffice-standard-home-edition-2.7.0-x86_64.rpm'
 anydesk='https://download.anydesk.com/linux/anydesk_6.3.0-1_x86_64.rpm'
 
-PROGRAMS_TO_INSTALL=(firefox yandex-browser-stable microsoft-edge-stable google-chrome-stable gimp r7-office master-pdf-editor thunderbird "$chromegost" "$anydesk" "$myoffice" snapd)
+PROGRAMS_TO_INSTALL=(firefox yandex-browser-stable microsoft-edge-stable google-chrome-stable gimp r7-office master-pdf-editor thunderbird "$chromegost" "$anydesk" "$myoffice" snapd flatpak)
 
 SNAPS_TO_INSTALL=(skype)
 
