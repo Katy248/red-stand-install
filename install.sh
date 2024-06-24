a831ed2038f32db463cd46a309b8f036079d19de
Katy248 <petrovanton247@gmail.com>
2024-06-24 14:32:42 +0300

    Add `--no-upgrade` parameter

--- install.sh
+++ install.sh
@@ -104,20 +104,27 @@ fi
 
 if [[ "$INSTALL_PROGRAMS" == 1 ]]; then
 
-    update_packages
-
+    if [[ "$NO_UPGRADE" == 0]]; then
+        update_packages
+    fi
 
     add_repositories "${ADDITIONAL_REPOSITORIES[@]}"
     install_packages "${PROGRAMS_TO_INSTALL[@]}"
-    update_packages
+    if [[ "$NO_UPGRADE" == 0]]; then
+        update_packages
+    fi
 
     setup_snap
     install_snaps "${SNAPS_TO_INSTALL[@]}"
 
     setup_flathub
-    update_flatpaks
+    if [[ "$NO_UPGRADE" == 0]]; then
+        update_flatpaks
+    fi
     install_flatpaks "${FLATPAKS_TO_INSTALL[@]}"
-    update_flatpaks
+    if [[ "$NO_UPGRADE" == 0]]; then
+        update_flatpaks
+    fi
 fi
 
 if [[ "$DISABLE_SCREENLOCKER" == 1 ]]; then
