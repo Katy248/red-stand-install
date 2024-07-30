59decf4cb8f4f8b2c55fc1cc66cd6af0631648a3
Katy248 <petrovanton247@gmail.com>
2024-07-30 15:21:46 +0300

    Add confirmation to scripts

--- scripts/one-script-install.sh
+++ scripts/one-script-install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+printf "This script will use sudo, continue?\n"
+read -r -p "[Y/n]: " answer
+if [[ "${answer^^}" == "N" ]]; then
+  printf "Canceled by user\n"
+  exit 0
+fi
+
 repository='https://github.com/Katy248/red-stand-install.git'
 dir='install-scripts'
 
