f9a0dc08a927054da3824afe9c3b80d22de602bd
Katy248 <petrovanton247@gmail.com>
2024-08-08 16:56:27 +0300

    Changed script

--- scripts/one-script-install.sh
+++ scripts/one-script-install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-printf "This script will use sudo, continue?\n"
-read -r -p "[Y/n]: " answer
-if [[ "${answer^^}" == "N" ]]; then
-  printf "Canceled by user\n"
-  exit 0
-fi
+# printf "This script will use sudo, continue?\n"
+# read -r -p "[Y/n]: " answer
+# if [[ "${answer^^}" == "N" ]]; then
+#   printf "Canceled by user\n"
+#   exit 0
+# fi
 
 repository='https://github.com/Katy248/red-stand-install.git'
 dir='install-scripts'
 
-sudo dnf install git -y
-sudo dnf install lolcat -y
+pkexec dnf install git -y
+pkexec dnf install lolcat -y
 
 mkdir "$dir"
 cd "$dir" || exit 1
 git clone "$repository" .
 
-sudo bash install.sh --install-programs --disable-screen-lock
+pkexec bash install.sh --install-programs --disable-screen-lock
 bash install.sh --add-shortcuts
