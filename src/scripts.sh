e2acddb89d31b0f88d4b7d38bb3012ddf1414437
Katy248 <petrovanton247@gmail.com>
2024-08-23 11:17:53 +0300

    Added error printing

--- src/scripts.sh
+++ src/scripts.sh
@@ -1,24 +1,96 @@
 #!/bin/bash
 
+# Format {{{1
+
+red=$(tput setaf 9)
+yellow=$(tput setaf 11)
+green=$(tput setaf 10)
+normal=$(tput sgr0)
+bold=$(tput bold)
+dim=$(tput dim)
+italic=$(tput sitm)
+reverse=$(tput rev)
+
+# }}}2
+
+print_header() {
+  local header_text=$1
+  local header_num=$2
+    printf "%s" "${bold}"
+    if [[ "${ENABLE_MD_FORMAT}" == 1 ]]; then
+        
+        header=""
+        for i in $(seq '1' "${header_num}"); do
+            header="${header}#"
+        done
+        
+        
+        printf "%s %s\n\n" "${header}" "${header_text}"
+    else
+        printf "%s\n" "${header_text}"
+    fi
+    printf "%s" "${normal}"
+}
+
+print_list() {
+    for item in "$@"
+    do
+        if [[ "${ENABLE_MD_FORMAT}" == 1 ]]; then
+            echo "- ${item}"
+        else
+            printf "    %s\n" "${item}"
+        fi
+    done
+    if [[ "${ENABLE_MD_FORMAT}" == 1 ]]; then
+        echo ""
+    fi
+}
+
+# Write error message to stderr
+#
+# Usage:
+#   print_error ERROR
+#
+# ERROR: error message
+print_error() {
+  local error=$1
+  echo "${red}ERROR: ${error}${normal}" >&2 # &2 is stderr
+}
+
+print_log() {
+    
+    color=${green}
+    if [[ "$2" == "WARN" ]]; then
+        color=${yellow}
+    fi
+    
+    if [[ "${DEBUG}" == 1 ]]; then
+        current_date=$(date +%T) || "current date"
+        echo "[${current_date}] ${color}${1}${normal}"
+    fi
+}
+
+# Install functions {{{
+
 update_packages() {
     dnf makecache
-    dnf upgrade -y
+    dnf upgrade -y || print_error "Cannot upgrade"
 }
 install_packages() {
     for pak in "$@"
     do
         print_log "Installing rpm package '${pak}'"
-        dnf install -y --skip-broken "${pak}"
+        dnf install -y --skip-broken "${pak}" || print_error "Cannot install package ${pak}"
     done
 }
 
 download_rpm() {
   local destdir
   destdir="$1"
-  print_log "Destination directory for rm download is: '${destdir}'"
+  print_log "Destination directory for rpm download is: '${destdir}'"
   for pak in "${@:2}"; do
     print_log "Downloading '${pak}'"
-    dnf download "${pak}" --destdir "${destdir}"
+    dnf download "${pak}" --destdir "${destdir}"  || "Cannot download package ${pak}"
   done
 }
 
@@ -29,7 +101,7 @@ install_snaps() {
     for pak in "$@"
     do
         print_log "Installing snap package '${pak}'"
-        snap install "${pak}"
+        snap install "${pak}" || print_error "Cannot install snap package ${pak}"
     done
 }
 setup_flathub() {
@@ -43,7 +115,7 @@ update_flatpaks() {
 install_flatpaks() {
     for pak in "$@"
     do
-        flatpak install "${pak}" -y
+        flatpak install "${pak}" -y || print_error "Cannot install flatpak package ${pak}"
     done
 }
 add_repositories() {
@@ -78,7 +150,7 @@ disable_screen_locking() {
         # disable autosuspend from battery
         gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type "nothing"
         
-        elif [[ "${XDG_CURRENT_DESKTOP}" == "KDE" ]]; then
+    elif [[ "${XDG_CURRENT_DESKTOP}" == "KDE" ]]; then
         
         lock_config="${HOME}/.config/kscreenlockerrc"
         
@@ -88,69 +160,11 @@ disable_screen_locking() {
         echo "Timeout=4" > "${lock_config}"
         echo "LockGrace=5" > "${lock_config}"
         
-        elif [[ "${XDG_CURRENT_DESKTOP}" == "MATE" ]]; then
+    elif [[ "${XDG_CURRENT_DESKTOP}" == "MATE" ]]; then
         
         # TODO: Make rules for MATE DE
         print_error "MATE Currently not supported for screenlock disabling"
     fi
 }
 
-# Format {{{
 
-red=$(tput setaf 9)
-yellow=$(tput setaf 11)
-green=$(tput setaf 10)
-normal=$(tput sgr0)
-bold=$(tput bold)
-dim=$(tput dim)
-italic=$(tput sitm)
-reverse=$(tput rev)
-
-# }}}
-
-print_header() {
-    printf "%s" "${bold}"
-    if [[ "${ENABLE_MD_FORMAT}" == 1 ]]; then
-        
-        header=""
-        for i in $(seq '1' "${2}"); do
-            header="${header}#"
-        done
-        
-        
-        printf "%s %s\n\n" "${header}" "${1}"
-    else
-        printf "%s\n" "${1}"
-    fi
-    printf "%s" "${normal}"
-}
-
-print_list() {
-    for item in "$@"
-    do
-        if [[ "${ENABLE_MD_FORMAT}" == 1 ]]; then
-            echo "- ${item}"
-        else
-            printf "    %s\n" "${item}"
-        fi
-    done
-    if [[ "${ENABLE_MD_FORMAT}" == 1 ]]; then
-        echo ""
-    fi
-}
-print_error() {
-    echo "${red}ERROR: $1${normal}" >&2 # &2 is stderr
-}
-
-print_log() {
-    
-    color=${green}
-    if [[ "$2" == "WARN" ]]; then
-        color=${yellow}
-    fi
-    
-    if [[ "${DEBUG}" == 1 ]]; then
-        current_date=$(date +%T) || "current date"
-        echo "[${current_date}] ${color}${1}${normal}"
-    fi
-}
