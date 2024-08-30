adb74d222e587162eb6ad0b09755e995358b72a9
Katy248 <petrovanton247@gmail.com>
2024-08-30 14:44:30 +0300

    Update scripts and docs

--- scripts/one-script-install.sh
+++ scripts/one-script-install.sh
@@ -5,6 +5,13 @@ install_programs() {
   sudo dnf install lolcat -y
 }
 
+printf "This script will use sudo, continue?\n"
+read -r -p "[Y/n]: " answer
+if [[ "${answer^^}" == "N" ]]; then
+  printf "Canceled by user\n"
+  exit 0
+fi
+
 repository='https://github.com/Katy248/red-stand-install.git'
 dir='install-scripts'
 
