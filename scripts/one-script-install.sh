9be0b58326b246a4a076b5cae5acf52a82186014
Katy248 <petrovanton247@gmail.com>
2024-08-30 14:39:15 +0300

    Updated one-scrip-install

--- scripts/one-script-install.sh
+++ scripts/one-script-install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 install_programs() {
-  dnf install git -y
-  dnf install lolcat -y
+  sudo dnf install git -y
+  sudo dnf install lolcat -y
 }
 
 repository='https://github.com/Katy248/red-stand-install.git'
 dir='install-scripts'
 
-pkexec install_programs
+install_programs
 
-mkdir -p "$dir"
-cd "$dir" || exit 1
-git clone "$repository" .
+mkdir -p "${dir}"
+cd "${dir}" || exit 1
+git clone "${repository}" .
 
 sudo bash install.sh --install-programs --disable-screen-lock
 bash install.sh --add-shortcuts
