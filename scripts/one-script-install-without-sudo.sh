925bb68691be9a8212f5d61d09b805881dbc5a92
Katy248 <petrovanton247@gmail.com>
2024-10-02 17:11:02 +0300

    Added script for installation without sudo enabled

---
+++ scripts/one-script-install-without-sudo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+install_programs() {
+    pkexec dnf install git lolcat -y
+}
+
+repository='https://github.com/Katy248/red-stand-install.git'
+start_dir=$(pwd)
+dir="${start_dir}/install-scripts"
+install_programs
+
+mkdir -p "${dir}"
+cd "${dir}" || exit 1
+git clone "${repository}" .
+
+pkexec bash "${dir}/install.sh" --install-programs
+bash install.sh --add-shortcuts --disable-screen-lock
